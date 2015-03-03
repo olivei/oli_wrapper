006d916b46d23a4287ac72455f37268b66a41b76
oliveirf <fransol@sapo.pt>
2015-03-03 17:50:42 -0500

    init

---
+++ oli_wrapper-master/metadata.rb
@@ -0,0 +1,9 @@
+name             'stepking_wrapper'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stepking_wrapper'
+long_description 'Installs/Configures stepking_wrapper'
+version          '0.1.0'
+
+depends "stepking_remotefile"
---
+++ oli_wrapper-master/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: stepking_wrapper
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "stepking_remotefile"
